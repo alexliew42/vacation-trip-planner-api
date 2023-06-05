912e9ab2e7ab3f1c6a1d666c63770850139c93ee
Alex Liew <liewtwoo42@gmail.com>
2023-06-05 13:59:13 -0400

    create jbuilder, create trips controller, add trips index action

---
+++ app/views/trips/_trip.json.jbuilder
@@ -0,0 +1,4 @@
+json.id trip.id
+json.title trip.title
+json.start_time trip.start_time
+json.end_time trip.end_time
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  
+  get "/trips" => "trips#index"
   post "/users" => "users#create"
 end
